7ec7431691a51b83fa837cd05e7026b256409dac
lambiengcode <lambiengcode@gmail.com>
2024-06-02 13:22:25 +0700

    update .

---
+++ google_sign_in_all_platforms/example/lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:example/home.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const HomeScreen(),
+    );
+  }
+}
